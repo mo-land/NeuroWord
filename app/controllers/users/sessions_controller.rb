c78f3e5c655aa97d1be04a52f8955b32b055346d
mo-land <oh.peach.march@gmail.com>
2025-07-02 16:24:52 +0900

    fix:lint修正0702②

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 class Users::SessionsController < Devise::SessionsController
-  before_action :configure_sign_in_params, only: [:create]
+  before_action :configure_sign_in_params, only: [ :create ]
 
   # GET /resource/sign_in
   # def new
@@ -22,7 +22,7 @@ class Users::SessionsController < Devise::SessionsController
 
   # If you have extra params to permit, append them to the sanitizer.
   def configure_sign_in_params
-    devise_parameter_sanitizer.permit(:sign_in, keys: [:attribute])
+    devise_parameter_sanitizer.permit(:sign_in, keys: [ :attribute ])
   end
 
   # サインイン後のリダイレクト先を指定する
@@ -30,7 +30,7 @@ class Users::SessionsController < Devise::SessionsController
     root_path
   end
 
-  #サインアウト後のリダイレクト先を指定
+  # サインアウト後のリダイレクト先を指定
   def after_sign_out_path_for(resource_or_scope)
     new_user_session_path
   end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users, controllers: {
-    registrations: 'users/registrations'
+    registrations: "users/registrations"
   }
   # resources :static_pages
   root "static_pages#top"
