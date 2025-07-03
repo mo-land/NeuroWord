03ec0c1a224af1cdfead810eb9c590d9509af7d5
mo-land <oh.peach.march@gmail.com>
2025-07-03 14:39:28 +0900

    add:CardSetsのルーティングを追加

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,9 @@ Rails.application.routes.draw do
   # resources :static_pages
   root "static_pages#top"
 
-  resources :questions, only: %i[index new create show]
+  resources :questions, only: %i[index new create show] do
+    resources :card_sets
+  end
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
