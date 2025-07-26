b17a2cf646dce381d37ef56733c5783dcf758d79
mo-land <oh.peach.march@gmail.com>
2025-07-26 17:26:33 +0900

    add:コントローラーにタグ関連の記述を追記

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,7 +1,9 @@
 class QuestionsController < ApplicationController
-  before_action :authenticate_user!, except: [ :index, :show ]
+  before_action :authenticate_user!, except: [ :index, :show, :search_tag ]
 
-  def index; end
+  def index
+    @tag_list = Tag.all
+  end
 
   def new
     @question = Question.new
@@ -9,7 +11,9 @@ class QuestionsController < ApplicationController
 
   def create
     @question = current_user.questions.build(question_params)
+    tag_list = params[:question][:tag].split(",")
     if @question.save
+      @question.save_tag(tag_list)
       # ステップ2（CardSet追加）にリダイレクト
       redirect_to new_question_card_set_path(@question),
                   notice: "ステップ1完了！カードセットを追加してください（2組以上）"
@@ -21,15 +25,19 @@ class QuestionsController < ApplicationController
 
   def show
     @question = Question.find(params[:id])
+    @question_tags = @question.tags
   end
 
   def edit
     @question = current_user.questions.find(params[:id])
+    @tag_list = @question.tags.pluck(:name).join(",")
   end
 
   def update
   @question = current_user.questions.find(params[:id])
+  tag_list = params[:question][:tag].split(",")
     if @question.update(question_params)
+      @question.save_tag(tag_list)
       redirect_to question_path(@question), success: t("defaults.flash_message.updated", item: Question.model_name.human)
     else
       flash.now[:danger] = t("defaults.flash_message.not_updated", item: Question.model_name.human)
@@ -43,9 +51,24 @@ class QuestionsController < ApplicationController
     redirect_to questions_path, success: t("defaults.flash_message.deleted", item: Question.model_name.human), status: :see_other
   end
 
+  def search_tag
+    @tag_list = Tag.all
+    # パラメータ名を明示的に指定
+    @tag = Tag.find_by(name: params[:tags_name])
+    if @tag
+      @questions = @tag.questions.includes(:user, :tags).order(created_at: :desc).page(params[:page])
+    else
+      @questions = Question.none
+    end
+  end
+
   private
 
   def question_params
     params.require(:question).permit(:title, :description)
+  end
+
+  def tag_params
+    params.require(:question).permit(:name)
   end
 end
