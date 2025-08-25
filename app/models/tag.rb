7eb0719843d95641ca3bdeec14cc2654bd3635c5
mo-land <oh.peach.march@gmail.com>
2025-08-25 15:29:10 +0900

    fix:タグの長さを25字以内→20字以内に修正

--- app/models/tag.rb
+++ app/models/tag.rb
@@ -1,5 +1,5 @@
 class Tag < ApplicationRecord
-  validates :name, presence: true, length: { maximum: 25 }
+  validates :name, presence: true, length: { maximum: 20 }
   has_many :question_tags, dependent: :destroy
   has_many :questions, through: :question_tags
 end
