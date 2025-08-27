a2ff3a14ef1267598d3d28de69b42ad43f6af51d
mo-land <oh.peach.march@gmail.com>
2025-08-27 11:08:04 +0900

    add:プロフィール関連のルーティング・コントローラー・アクションを追加

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,18 @@
+class UsersController < ApplicationController
+  before_action :authenticate_user!
+  before_action :set_user
+
+  def mypage
+    @questions = @user.questions.includes([:tags]).page(params[:page])
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :email)
+  end
+
+  def set_user
+    @user = current_user
+  end
+end
