d11bf34f88535f5aee03982cdb71ef73cabae6ba
mo-land <oh.peach.march@gmail.com>
2025-09-02 20:30:43 +0900

    add: request#indexアクションを作成

--- app/controllers/requests_controller.rb
+++ app/controllers/requests_controller.rb
@@ -1,2 +1,8 @@
 class RequestsController < ApplicationController
+  before_action :authenticate_user!, except: %i[ index ]
+
+  def index
+    @requests = Request.includes(:user, :question).page(params[:game_records_page]).per(5)
+    @current_requests_tab = params[:tab] || "user_requests"
+  end
 end
