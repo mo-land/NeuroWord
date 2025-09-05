6d32effc73959c704d096e1fb917ff019c64b76e
mo-land <oh.peach.march@gmail.com>
2025-09-05 11:13:13 +0900

    add: コントローラーにrequests#createアクションを追加

--- app/controllers/requests_controller.rb
+++ app/controllers/requests_controller.rb
@@ -7,5 +7,22 @@ class RequestsController < ApplicationController
 
   def new
     @request = Request.new
+    @search = Question.ransack(params[:q])
+  end
+
+  def create
+    @request = current_user.requests.build(request_params)
+    if @request.save
+      redirect_to requests_path, success: t("defaults.flash_message.created", item: Request.model_name.human)
+    else
+      flash.now[:danger] = t("defaults.flash_message.not_created", item: Request.model_name.human)
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def request_params
+    params.require(:request).permit(:title, :content, :question_id)
   end
 end
