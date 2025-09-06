19288a14310a1f0c549a0c06493e70dc0c24c92b
mo-land <oh.peach.march@gmail.com>
2025-09-07 01:40:38 +0900

    delete: Applicationコントローラーから修正依頼のransack用アクションを削除（requests#indexに移動するため）

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,16 +2,10 @@ class ApplicationController < ActionController::Base
   # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
   allow_browser versions: :modern
   before_action :set_search
-  before_action :set_search_requests
 
   def set_search
     # @search = Question.search(params[:q])
     @search = Question.ransack(params[:q]) # ransackメソッド推奨
     @search_questions = @search.result(distinct: true).includes(:user, :tags).order(created_at: :desc).page(params[:page])
-  end
-
-  def set_search_requests
-    @search_request = Request.ransack(params[:q]) # ransackメソッド推奨
-    @search_requests = @search_request.result(distinct: true).includes(:user, :question).order(created_at: :desc).page(params[:page])
   end
 end
