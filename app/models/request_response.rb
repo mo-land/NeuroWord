39dc02966b9880d0048b91ffd1d95097a8c3b47d
mo-land <oh.peach.march@gmail.com>
2025-09-08 20:45:46 +0900

    delete: チェックボックス入力のバリデーションを削除

--- app/models/request_response.rb
+++ app/models/request_response.rb
@@ -3,7 +3,6 @@ class RequestResponse < ApplicationRecord
   belongs_to :user
 
   validates :content, presence: true, length: { maximum: 500 }
-  validates :is_completed, inclusion: { in: [ true, false ] }
 
   after_create :update_request_status_if_completed
 
