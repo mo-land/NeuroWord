b4cb3d1e19ffe1f9c7f082110279c83b259d921d
mo-land <oh.peach.march@gmail.com>
2025-09-24 19:48:54 +0900

    update: 認証機能関連のテストケースをシステムスペックに切り出し

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -28,32 +28,6 @@ RSpec.describe User, type: :model do
     end
   end
 
-  describe 'Deviseの認証機能' do
-    context 'ユーザー登録' do
-      it '有効な情報でユーザーを作成できる' do
-      end
-
-      it '重複するメールアドレスでは作成できない' do
-      end
-    end
-
-    context 'ログイン機能' do
-      it '正しい認証情報でログインできる' do
-      end
-
-      it '間違った認証情報ではログインできない' do
-      end
-    end
-
-    context 'パスワードリセット機能' do
-      it 'パスワードリセットトークンが生成される' do
-      end
-
-      it '有効なトークンでパスワードを変更できる' do
-      end
-    end
-  end
-
   describe 'アソシエーションのテスト' do
     context 'questionsとの関係' do
       it '複数のquestionsを持つことができる' do
--- spec/system/users_spec.rb
+++ spec/system/users_spec.rb
@@ -5,5 +5,29 @@ RSpec.describe "Users", type: :system do
     driven_by(:rack_test)
   end
 
-  pending "add some scenarios (or delete) #{__FILE__}"
+  describe 'Deviseの認証機能' do
+    context 'ユーザー登録' do
+      it '有効な情報でユーザーを作成できる' do
+      end
+
+      it '重複するメールアドレスでは作成できない' do
+      end
+    end
+
+    context 'ログイン機能' do
+      it '正しい認証情報でログインできる' do
+      end
+
+      it '間違った認証情報ではログインできない' do
+      end
+    end
+
+    context 'パスワードリセット機能' do
+      it 'パスワードリセットトークンが生成される' do
+      end
+
+      it '有効なトークンでパスワードを変更できる' do
+      end
+    end
+  end
 end
