7a22a42389bcc57443bf5a563762ae7bde2d6122
mo-land <oh.peach.march@gmail.com>
2025-09-26 11:21:00 +0900

    update: CardSetモデルのユーザー操作に関連する部分をシステムスペックに切り出し

--- spec/models/card_set_spec.rb
+++ spec/models/card_set_spec.rb
@@ -77,64 +77,6 @@ RSpec.describe CardSet, type: :model do
     end
   end
 
-  describe "#all_cards_shuffled" do
-    context "起点カードと関連語カードが存在する場合" do
-      it "起点カードと関連語カードを含む全カードを取得できる" do
-      end
-
-      it "各カードに適切な属性が設定される" do
-      end
-
-      it "取得したカードがシャッフルされている" do
-      end
-    end
-
-    context "関連語が存在しない場合" do
-      it "起点カードのみを含む配列を返す" do
-      end
-    end
-  end
-
-  describe "#origin_card_data" do
-    context "起点カードデータの生成" do
-      it "適切な属性を持つハッシュを返す" do
-      end
-
-      it "wordにorigin_wordが設定される" do
-      end
-
-      it "typeに:originが設定される" do
-      end
-
-      it "set_idにカードセットのIDが設定される" do
-      end
-
-      it "css_classに'origin-card'が設定される" do
-      end
-    end
-  end
-
-  describe "#related_cards_data" do
-    context "関連語カードデータの生成" do
-      it "全ての関連語がカードデータに変換される" do
-      end
-
-      it "各カードに適切な属性が設定される" do
-      end
-
-      it "typeに:relatedが設定される" do
-      end
-
-      it "css_classに'related-card'が設定される" do
-      end
-    end
-
-    context "関連語が存在しない場合" do
-      it "空の配列を返す" do
-      end
-    end
-  end
-
   describe "#cards_count" do
     context "関連語が存在する場合" do
       it "起点カード1枚＋関連語カード数の合計を返す" do
@@ -153,33 +95,6 @@ RSpec.describe CardSet, type: :model do
 
     context "関連語がnilの場合" do
       it "1を返す" do
-      end
-    end
-  end
-
-  describe "#includes_related_word?" do
-    context "指定した単語が関連語に含まれる場合" do
-      it "trueを返す" do
-      end
-    end
-
-    context "指定した単語が関連語に含まれない場合" do
-      it "falseを返す" do
-      end
-    end
-
-    context "関連語が空の場合" do
-      it "falseを返す" do
-      end
-    end
-
-    context "関連語がnilの場合" do
-      it "falseを返す" do
-      end
-    end
-
-    context "引数がnilの場合" do
-      it "falseを返す" do
       end
     end
   end
--- spec/system/card_sets_spec.rb
+++ spec/system/card_sets_spec.rb
@@ -5,5 +5,88 @@ RSpec.describe "CardSets", type: :system do
     driven_by(:rack_test)
   end
 
-  pending "add some scenarios (or delete) #{__FILE__}"
+  describe "#all_cards_shuffled" do
+    context "起点カードと関連語カードが存在する場合" do
+      it "起点カードと関連語カードを含む全カードを取得できる" do
+      end
+
+      it "各カードに適切な属性が設定される" do
+      end
+
+      it "取得したカードがシャッフルされている" do
+      end
+    end
+
+    context "関連語が存在しない場合" do
+      it "起点カードのみを含む配列を返す" do
+      end
+    end
+  end
+
+  describe "#origin_card_data" do
+    context "起点カードデータの生成" do
+      it "適切な属性を持つハッシュを返す" do
+      end
+
+      it "wordにorigin_wordが設定される" do
+      end
+
+      it "typeに:originが設定される" do
+      end
+
+      it "set_idにカードセットのIDが設定される" do
+      end
+
+      it "css_classに'origin-card'が設定される" do
+      end
+    end
+  end
+
+  describe "#related_cards_data" do
+    context "関連語カードデータの生成" do
+      it "全ての関連語がカードデータに変換される" do
+      end
+
+      it "各カードに適切な属性が設定される" do
+      end
+
+      it "typeに:relatedが設定される" do
+      end
+
+      it "css_classに'related-card'が設定される" do
+      end
+    end
+
+    context "関連語が存在しない場合" do
+      it "空の配列を返す" do
+      end
+    end
+  end
+  
+  describe "#includes_related_word?" do
+    context "指定した単語が関連語に含まれる場合" do
+      it "trueを返す" do
+      end
+    end
+
+    context "指定した単語が関連語に含まれない場合" do
+      it "falseを返す" do
+      end
+    end
+
+    context "関連語が空の場合" do
+      it "falseを返す" do
+      end
+    end
+
+    context "関連語がnilの場合" do
+      it "falseを返す" do
+      end
+    end
+
+    context "引数がnilの場合" do
+      it "falseを返す" do
+      end
+    end
+  end
 end
