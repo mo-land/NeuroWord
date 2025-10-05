215ad78428d6e0709292f29803db883fd8ba77ee
mo-land <oh.peach.march@gmail.com>
2025-10-05 12:01:41 +0900

    fix: lint修正1005①

--- db/migrate/20251005024949_create_related_words.rb
+++ db/migrate/20251005024949_create_related_words.rb
@@ -6,6 +6,6 @@ class CreateRelatedWords < ActiveRecord::Migration[7.2]
 
       t.timestamps
     end
-    add_index :related_words, [:origin_word_id, :related_word], unique: true
+    add_index :related_words, [ :origin_word_id, :related_word ], unique: true
   end
 end
