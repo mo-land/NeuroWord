789542088a40d65969ad1e32bb8e9d8e8af2b08d
mo-land <oh.peach.march@gmail.com>
2025-10-05 12:03:36 +0900

    add: モデルにorigin_words・related_words関連のバリデーション・アソシエーションを追加

--- app/models/origin_word.rb
+++ app/models/origin_word.rb
@@ -1,3 +1,6 @@
 class OriginWord < ApplicationRecord
   belongs_to :question
+  has_many :related_words, dependent: :destroy
+
+  validates :origin_word, presence: true, length: { maximum: 50 }
 end
