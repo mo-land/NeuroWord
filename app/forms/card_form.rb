bde114f3734d3da969d4c4342e1f7bef45391726
mo-land <oh.peach.march@gmail.com>
2025-10-06 15:32:13 +0900

    add: フォームオブジェクトにsaveを定義

--- app/forms/card_form.rb
+++ app/forms/card_form.rb
@@ -8,9 +8,49 @@ class CardForm
 
   validates :origin_word, presence: true, length: { maximum: 50 }
   validates :related_word, presence: true, length: { maximum: 100 }
+  validate :question_total_cards_limit
   validate :related_word_uniqueness_within_question
 
+  def save
+    return false unless valid?
+
+    question = Question.find(question_id)
+    @origin_word_record = question.origin_words.create(origin_word: origin_word)
+
+    if @origin_word_record.persisted?
+      @origin_word_record.related_words.create(related_word: related_word)
+      @origin_word_record
+    else
+      # 起点語の保存に失敗した場合、エラーをこのフォームオブジェクトに転記
+      @origin_word_record.errors.full_messages.each do |message|
+        errors.add(:base, message)
+      end
+      false
+    end
+  end
+
+  def origin_word_id
+    @origin_word_record&.id
+  end
+
+  def words_count
+    1 + (related_words&.compact_blank&.count || 0)
+  end
+
   private
+
+  def question_total_cards_limit
+    return unless question_id.present?
+
+    question = Question.find(question_id)
+    current_total = question.total_cards_count
+    new_words_count = 2 # 起点語1 + 関連語1
+    total_count = current_total + new_words_count
+
+    if total_count > 10
+      errors.add(:base, "このカードセットを追加すると総カード数が10枚を超えます（予想枚数：#{total_count}枚）")
+    end
+  end
 
   def related_word_uniqueness_within_question
     return unless question_id.present? && related_word.present?
