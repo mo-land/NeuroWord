86fdc69c0cc56bd06c704d4ed78647228897c560
mo-land <oh.peach.march@gmail.com>
2025-10-09 11:28:49 +0900

    fix: ワードセット編集時の関連語追加ボタン表示条件を追加

--- app/models/question.rb
+++ app/models/question.rb
@@ -88,9 +88,20 @@ class Question < ApplicationRecord
     RelatedWord.joins(:origin_word).where(origin_words: { question_id: id }).count
   end
 
+  def can_add_card_set?(origin_word_count = 1, related_words_count = 0)
+    (total_cards_count + origin_word_count + related_words_count) <= 10
+  end
+
   def can_add_related_word?(context: :related_words)
-    # カードセット追加画面では8枚まで、関連語追加画面では9枚まで
-    max_count = context == :card_sets_new ? 8 : 9
+    # カードセット追加画面では8枚まで、編集画面では10枚まで、関連語追加画面では9枚まで
+    max_count = case context
+    when :card_sets_new
+                  8
+    when :card_sets_edit
+                  10
+    else
+                  9
+    end
     return false if total_cards_count >= max_count
 
     if origin_words.count == 1
