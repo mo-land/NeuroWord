781eb30ea4aa0e7ff0ad8594904945e23644fd38
mo-land <oh.peach.march@gmail.com>
2025-10-13 10:38:56 +0900

    add: SnsCrendentialモデルの作成

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2025_10_05_024949) do
+ActiveRecord::Schema[7.2].define(version: 2025_10_11_084310) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -98,6 +98,15 @@ ActiveRecord::Schema[7.2].define(version: 2025_10_05_024949) do
     t.index ["user_id"], name: "index_requests_on_user_id"
   end
 
+  create_table "sns_credentials", force: :cascade do |t|
+    t.string "provider"
+    t.string "uid"
+    t.bigint "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_sns_credentials_on_user_id"
+  end
+
   create_table "tags", force: :cascade do |t|
     t.string "name", null: false
     t.datetime "created_at", null: false
@@ -130,4 +139,5 @@ ActiveRecord::Schema[7.2].define(version: 2025_10_05_024949) do
   add_foreign_key "request_responses", "users"
   add_foreign_key "requests", "questions"
   add_foreign_key "requests", "users"
+  add_foreign_key "sns_credentials", "users"
 end
