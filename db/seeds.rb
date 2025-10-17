4b6e8c590c6338910b9a4558e77fc5f07da3f2f8
mo-land <oh.peach.march@gmail.com>
2025-10-17 19:57:46 +0900

    fix: lint修正1017②

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,14 +9,14 @@
 #   end
 
 fe, rails, js, css, git, it, others, breath_or_uncategorized  = Category.create([
-  {name: '基本情報'},
-  {name: 'Ruby on Rails'},
-  {name: 'JavaScript'},
-  {name: 'CSS'},
-  {name: 'Git'},
-  {name: 'その他学習（IT系）'},
-  {name: 'その他学習（IT以外）'},
-  {name: '息抜き系・未分類'}
+  { name: '基本情報' },
+  { name: 'Ruby on Rails' },
+  { name: 'JavaScript' },
+  { name: 'CSS' },
+  { name: 'Git' },
+  { name: 'その他学習（IT系）' },
+  { name: 'その他学習（IT以外）' },
+  { name: '息抜き系・未分類' }
   ])
 
 
@@ -24,19 +24,19 @@ technology, management, strategy = fe.children.create(
   [
     { name: 'テクノロジ系' },
     { name: 'マネジメント系' },
-    { name: 'ストラテジ系' },
+    { name: 'ストラテジ系' }
   ]
 )
 
-['基礎理論', 'コンピュータシステム', '技術要素_データベース', '技術要素_ネットワーク', '技術要素_セキュリティ', '技術要素_その他', '開発技術'].each do |name|
+[ '基礎理論', 'コンピュータシステム', '技術要素_データベース', '技術要素_ネットワーク', '技術要素_セキュリティ', '技術要素_その他', '開発技術' ].each do |name|
   technology.children.create(name: name)
 end
 
-['プロジェクトマネジメント', 'サービスマネジメント'].each do |name|
+[ 'プロジェクトマネジメント', 'サービスマネジメント' ].each do |name|
   management.children.create(name: name)
 end
 
-['システム戦略', '経営戦略', '企業と法務'].each do |name|
+[ 'システム戦略', '経営戦略', '企業と法務' ].each do |name|
   strategy.children.create(name: name)
 end
 
