884a01ba240fae222102495a9fc757ea75340453
mo-land <oh.peach.march@gmail.com>
2025-10-17 17:45:49 +0900

    add: seedsにカテゴリ生成のための記述を追加

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,16 +7,53 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
-10.times do
-  User.create!(name: Faker::Name.name,
-              email: Faker::Internet.unique.email,
-              password: "password",
-              password_confirmation: "password")
+
+fe, rails, js, css, git, it, others, breath_or_uncategorized  = Category.create([
+  {name: '基本情報'},
+  {name: 'Ruby on Rails'},
+  {name: 'JavaScript'},
+  {name: 'CSS'},
+  {name: 'Git'},
+  {name: 'その他学習（IT系）'},
+  {name: 'その他学習（IT以外）'},
+  {name: '息抜き系・未分類'}
+  ])
+
+
+technology, management, strategy = fe.children.create(
+  [
+    { name: 'テクノロジ系' },
+    { name: 'マネジメント系' },
+    { name: 'ストラテジ系' },
+  ]
+)
+
+['基礎理論', 'コンピュータシステム', '技術要素_データベース', '技術要素_ネットワーク', '技術要素_セキュリティ', '技術要素_その他', '開発技術'].each do |name|
+  technology.children.create(name: name)
 end
 
-user_ids = User.ids
-
-20.times do |index|
-  user = User.find(user_ids.sample)
-  user.questions.create!(title: "タイトル#{index}", description: "本文#{index}")
+['プロジェクトマネジメント', 'サービスマネジメント'].each do |name|
+  management.children.create(name: name)
 end
+
+['システム戦略', '経営戦略', '企業と法務'].each do |name|
+  strategy.children.create(name: name)
+end
+
+# -------------------------------
+
+# 10.times do
+#   User.create!(name: Faker::Name.name,
+#   email: Faker::Internet.unique.email,
+#   password: "password",
+#   password_confirmation: "password")
+# end
+
+# user_ids = User.ids
+
+# 20.times do |index|
+#   user = User.find(user_ids.sample)
+#   user.questions.create!(title: "タイトル#{index}", description: "本文#{index}")
+# end
+
+# -------------------------------
