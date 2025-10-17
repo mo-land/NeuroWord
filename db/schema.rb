58f257bde0612ccb7cfc9e0b4c1d7580bde2c0c1
mo-land <oh.peach.march@gmail.com>
2025-10-17 17:31:55 +0900

    add: categoriesテーブルを追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2025_10_11_084310) do
+ActiveRecord::Schema[7.2].define(version: 2025_10_17_082957) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -21,6 +21,13 @@ ActiveRecord::Schema[7.2].define(version: 2025_10_11_084310) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["question_id"], name: "index_card_sets_on_question_id"
+  end
+
+  create_table "categories", force: :cascade do |t|
+    t.string "name", null: false
+    t.string "ancestry"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "game_records", force: :cascade do |t|
