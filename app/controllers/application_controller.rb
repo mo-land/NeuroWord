fb21b4d99014b2016b2816c7c1ca11f55c056afb
mo-land <oh.peach.march@gmail.com>
2025-10-17 20:59:42 +0900

    delete: application_controller.rbからransack設定を削除

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,11 +1,4 @@
 class ApplicationController < ActionController::Base
   # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
   allow_browser versions: :modern
-  before_action :set_search
-
-  def set_search
-    # @search = Question.search(params[:q])
-    @search = Question.ransack(params[:q]) # ransackメソッド推奨
-    @search_questions = @search.result(distinct: true).includes(:user, :tags).order(created_at: :desc).page(params[:page])
-  end
 end
