152da61fbbcea97c2dbf0d37c85f5f52fa683618
mo-land <oh.peach.march@gmail.com>
2025-11-21 11:44:24 +0900

    update: Google OAuth2の失敗メッセージを改善

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -26,28 +26,25 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
   # You should also create an action method in this controller like this:
   # def twitter
   # end
-
+  
   # More info at:
   # https://github.com/heartcombo/devise#omniauth
-
+  
   # GET|POST /resource/auth/twitter
   # def passthru
   #   super
   # end
-
+  
   # GET|POST /users/auth/twitter/callback
-  # def failure
-  #   super
-  # end
+  def failure
+    redirect_to root_path
+    set_flash_message(:notice, :failure, kind: "Google") if is_navigational_format?
+  end
 
-  protected
+  # protected
 
     # The path used when OmniAuth fails
     # def after_omniauth_failure_path_for(scope)
     #   super(scope)
     # end
-
-    def failure
-      redirect_to root_path and return
-    end
 end
