0506dc68d57246304dd363203294532a865c18b7
mo-land <oh.peach.march@gmail.com>
2025-11-27 15:45:44 +0900

    fix: 共有メモリ不足対策のため、オプションでソフトウェアラスタライザ・拡張機能を無効化

--- spec/support/capybara.rb
+++ spec/support/capybara.rb
@@ -13,6 +13,8 @@ Capybara.register_driver :selenium_chrome_headless do |app|
   options.add_argument('--no-sandbox')
   options.add_argument('--disable-dev-shm-usage')
   options.add_argument('--disable-gpu')
+  options.add_argument('--disable-software-rasterizer')
+  options.add_argument('--disable-extensions')
   options.add_argument('--window-size=1680,1050')
   Capybara::Selenium::Driver.new(app, browser: :chrome, options: options)
 end
