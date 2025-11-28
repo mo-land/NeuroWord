4ec0bdbe922752b3608f977069a513fa0c0738ad
mo-land <oh.peach.march@gmail.com>
2025-11-28 17:29:48 +0900

    fix: リスト作成・編集のモーダルを再設定

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -6,18 +6,13 @@ class ListsController < ApplicationController
 
   def new
     @list = current_user.lists.new
+    @current_list_id = params[:list_id]
   end
 
   def create
     @list = current_user.lists.new(list_params)
     if @list.save
-      respond_to do |format|
-        format.turbo_stream do
-          flash.now[:notice] = "リストを作成しました！"
-          render turbo_stream: turbo_stream.update("flash", partial: "shared/flash_messages")
-        end
-        format.html { redirect_to mypage_user_path(tab: "user_lists", list_id: @list.id), notice: "リストを作成しました！" }
-      end
+      redirect_to mypage_user_path(tab: "user_lists", list_id: @list.id), notice: "リストを作成しました！"
     else
       render :new, status: :unprocessable_entity
     end
@@ -28,17 +23,7 @@ class ListsController < ApplicationController
 
   def update
     if @list.update(list_params)
-      respond_to do |format|
-        format.turbo_stream do
-          flash.now[:notice] = "リストを更新しました！"
-          @lists = current_user.lists
-          render turbo_stream: [
-            turbo_stream.update("flash", partial: "shared/flash_messages"),
-            turbo_stream.update("list_selector_container", partial: "users/list_selector", locals: { lists: @lists, selected_list_id: @list.id })
-          ]
-        end
-        format.html { redirect_to mypage_user_path(tab: "user_lists", list_id: @list.id), notice: "リストを更新しました！" }
-      end
+      redirect_to mypage_user_path(tab: "user_lists", list_id: @list.id), notice: "リストを更新しました！"
     else
       render :edit, status: :unprocessable_entity
     end
@@ -46,7 +31,7 @@ class ListsController < ApplicationController
 
   def destroy
     @list.destroy!
-    redirect_to mypage_user_path(tab: 'user_lists'), notice: t("defaults.flash_message.deleted", item: List.model_name.human), status: :see_other
+    redirect_to mypage_user_path(tab: "user_lists"), notice: t("defaults.flash_message.deleted", item: List.model_name.human), status: :see_other
   end
 
   def batch_play
